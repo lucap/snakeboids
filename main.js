8fa9b274eee2d5f11374133628aff03ab063f37a
Luca Pellicoro <luca@hipmunk.com>
2016-09-10 00:56:35 -0700

    random snakes

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 
 var GRID_SIZE = 100
-var DIRECTIONS = {
-    up:    [-1, 0],
-    down:  [1, 0],
-    right: [0, 1],
-    left:  [0, -1],
-}
+var NUM_SNAKES = 20
+var SNAKE_LENGTH = 10
+var DIRECTIONS = [
+    [-1, 0], // up
+    [1, 0],  // down
+    [0, 1],  // right
+    [0, -1], // left
+]
 
 var snakes = []
 
 function cell_to_element(cell) {
     return $('table').find('tr').eq(cell[0]).find('td').eq(cell[1]);
+}
+
+function random_element(elements) {
+    return elements[Math.floor(Math.random()*elements.length)];
 }
 
 function random_empty_cell() {
@@ -23,19 +29,33 @@ function random_empty_cell() {
 }
 
 function draw_loop() {
-    move_snake(snakes[0], 'up');
+    for (var snake of snakes) {
+        move_snake(snake);
+    }
     window.requestAnimationFrame(draw_loop);
 }
 
-function move_snake(snake, direction) {
-    var head = snake[0];
-    var move_vector = DIRECTIONS[direction];
-    var new_head = [head[0] + move_vector[0], head[1] + move_vector[1]]
-    snake.unshift(new_head); // insert in the front
-    var tail = snake.pop();  // remove from the back
+function init_snakes() {
+    for (i = 0; i < NUM_SNAKES; i++) {
+        snakes.push({
+            heading: random_element(DIRECTIONS),
+            length: SNAKE_LENGTH,
+            cells: [random_empty_cell()],
+        });
+    }
+}
 
-    cell_to_element(tail).removeClass('snake')
+function move_snake(snake) {
+    var head = snake.cells[0];
+    var direction = snake.heading;
+    var new_head = [head[0] + direction[0], head[1] + direction[1]]
+    snake.cells.unshift(new_head); // insert in the front
     cell_to_element(new_head).addClass('snake')
+
+    while (snake.cells.length > snake.length) {
+        var tail = snake.cells.pop();  // remove from the back
+        cell_to_element(tail).removeClass('snake');
+    }
 }
 
 function draw_grid() {
@@ -51,12 +71,6 @@ function draw_grid() {
 
 $(document).ready(function() {
     draw_grid();
-    var start_cell = random_empty_cell();
-    snakes.push([
-        start_cell,
-        [start_cell[0]+1, start_cell[1]],
-        [start_cell[0]+2, start_cell[1]],
-        [start_cell[0]+3, start_cell[1]],
-    ]);
+    init_snakes();
     window.requestAnimationFrame(draw_loop);
 });
